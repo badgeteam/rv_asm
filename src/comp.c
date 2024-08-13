7322dd5ab45470436a0c34cc768ff822bc6b8261
Matthias Braun <megaschaf@ha.si>
2024-08-13 03:41:34 +0200

    Beginning of Compiler Pass. Currently detectiong .text

--- src/comp.c
+++ src/comp.c
@@ -3,23 +3,33 @@
 #include"util.h"
 
 void compPass(CompContext*ctx){
+  bool mode_text=false,mode_data=false;
   while(ctx->token){
-    switch(ctx->token->type){
-      case Space:
-      case Comment:
-      case Newline:
+    if(ctx->token->type == Newline){
+      ctx->token = ctx->token->next;
+    }
+    else if(ctx->token->type == Identifier){
+      if(ctx->token->next && ctx->token->next->type == Doubledot){
+      // Symbol
+      // TODO Add Symbol
+      ctx->token = ctx->token->next->next;
+      break;
+      }
+      // Directive
+      else if(tokenIdentComp(".text",ctx->token)){
+	// TODO Select Section
+  printf(".text\n");
+	mode_text = true;
 	ctx->token=ctx->token->next;
-	break;
-      case Identifier:
-	if(ctx->token->next && ctx->token->next->type == Doubledot){
-	  // Symbol
-	}else if(ctx->token->buff[0] == '.'){
-	  // Directive
-	}else{
-	  // Statements
-	}
-      default:
+      }
+
+      else if(mode_text){ctx->token = ctx->token->next;}
+      else if(mode_data){ctx->token = ctx->token->next;}
+      else
 	compError("Unexpected Token in Main Switch",ctx->token);
+    }
+    else {
+      compError("Unexpected Token in Main Switch",ctx->token);
     }
   }
 }
@@ -28,14 +38,13 @@ void compPass(CompContext*ctx){
 CompContext*comp(char*filename){
   CompContext*ctx = malloc(sizeof(CompContext));
   ctx->tokenHead = tokenizeFile(filename);
-  printTokenInfo(ctx->tokenHead);
   ctx->tokenHead = pruneTokenTypes(ctx->tokenHead,Space|Comment);
-
   printTokenInfo(ctx->tokenHead);
-//  ctx->token = ctx->tokenHead;
-//  ctx->pass = INDEX;
 
-//  compPass(ctx);
+  ctx->token = ctx->tokenHead;
+  ctx->pass = INDEX;
+
+  compPass(ctx);
 
 //  ctx->token = ctx->tokenHead;
 //  ctx->pass = COMP;
